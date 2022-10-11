e0a66d5b1c61ac8c5a418c481a61ff8c76bdff6c
VickyParmar1987 <115558445+VickyParmar1987@users.noreply.github.com>
2022-10-11 21:36:42 +0530

    updated
    Updated..

--- DownJonesIndex/src/main/java/com/stock/DowJonesIndex/controller/StockOperationsController.java
+++ DownJonesIndex/src/main/java/com/stock/DowJonesIndex/controller/StockOperationsController.java
@@ -3,12 +3,11 @@
  */
 package com.stock.DowJonesIndex.controller;
 
-import java.io.IOException;
-import java.sql.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -35,29 +34,22 @@ public class StockOperationsController {
 	@Autowired
 	StockOperationsService stockOperationsService;
 	
-	@GetMapping(value = "/home")
-	public String getCurrentData(){
-		return "Hello!";
-		
-	}
 	
 	@GetMapping(value = "/{stock}")
 	public ResponseEntity<List<StockData>>  getStockData(@PathVariable("stock") String stock){
 		List<StockData> stockDataList = stockOperationsService.findByStock(stock);
 		
-		System.out.println("stockDataList:{}"+stockDataList.size());
 		return new ResponseEntity<>(stockDataList, HttpStatus.OK);
 		
 	}
 
 	
-	@PostMapping(value = "/bulk-insert")
-	public ResponseEntity<String> bulkInsertDataFile(@RequestParam("file") MultipartFile file){
-		
+	@PostMapping(value = "/bulk-insert", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ResponseEntity<String> bulkInsertDataFile(@RequestPart("file") MultipartFile file){
 		
 		try {
 			return new ResponseEntity<>(stockOperationsService.saveFile(file), HttpStatus.OK);
-		} catch (IOException e) {
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -66,7 +58,7 @@ public class StockOperationsController {
 	}
 	
 	
-	@PostMapping(value = "/jsonTest")
+	@PostMapping(value = "/")
 	public ResponseEntity<String> postStockData(@RequestBody  StockDataPOJO stockDataFromRequest){
 	
 		try {
